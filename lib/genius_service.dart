939693599150cf76d1a1e72ce10780b8b122b05d
COPE <aorefia2001@gmail.com>
2024-07-17 00:07:07 +0000

    Fix myrics api

--- lib/genius_service.dart
+++ lib/genius_service.dart
@@ -1,24 +1,32 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:html/parser.dart' show parse;
+import 'package:html/dom.dart';
 
 class GeniusService {
-  static const String _baseUrl = 'https://api.genius.com/';
-  static const String _accessToken = 'SAp3Q1c6spGDSg0Hi3Cb4XjF6PkSuHQFwbsqqkQGn9xclgc7Yws2mrlPX4wM4xHy'; // api key
+  static const String _baseUrl = 'api.genius.com';
+  static const String _accessToken = 'SAp3Q1c6spGDSg0Hi3Cb4XjF6PkSuHQFwbsqqkQGn9xclgc7Yws2mrlPX4wM4xHy'; // API key
 
   static Future<String?> fetchLyrics(String songTitle, String artist) async {
-    final searchUrl = '$_baseUrl/search?q=${Uri.encodeQueryComponent(songTitle + ' ' + artist)}';
-    final response = await http.get(Uri.parse(searchUrl), headers: {
+    final queryParameters = {'q': '$songTitle $artist'};
+    final url = Uri.https(_baseUrl, '/search', queryParameters);
+
+    final response = await http.get(url, headers: {
       'Authorization': 'Bearer $_accessToken',
     });
 
     if (response.statusCode == 200) {
       final data = json.decode(response.body);
-      final songData = data['response']['hits'][0]['result'];
-      final songUrl = songData['url'];
+      if (data['response']['hits'].isNotEmpty) {
+        final songData = data['response']['hits'][0]['result'];
+        final songUrl = songData['url'];
 
-      return _scrapeLyrics(songUrl);
+        return await _scrapeLyrics(songUrl);
+      } else {
+        return 'Paroles non trouvées';
+      }
     } else {
-      throw Exception('Failed to load lyrics: ${response.statusCode} ${response.body}');
+      throw Exception('Échec du chargement des paroles: ${response.statusCode} ${response.body}');
     }
   }
 
@@ -29,13 +37,23 @@ class GeniusService {
       final lyricsPage = response.body;
       return _extractLyricsFromHtml(lyricsPage);
     } else {
-      throw Exception('Failed to load lyrics from the song page: ${response.statusCode}');
+      throw Exception('Échec du chargement des paroles depuis la page de la chanson: ${response.statusCode}');
     }
   }
 
   static String? _extractLyricsFromHtml(String html) {
-    final RegExp regex = RegExp(r'<div class="lyrics">.*?<p>(.*?)</p>', dotAll: true);
-    final match = regex.firstMatch(html);
-    return match != null ? match.group(1) : 'Paroles non trouvées';
+    final document = parse(html);
+    final lyricsDiv = document.querySelector('div.lyrics') ?? document.querySelector('div[class*="Lyrics__Root"]');
+
+    if (lyricsDiv != null) {
+      return lyricsDiv.text.trim();
+    } else {
+      return 'Paroles non trouvées';
+    }
   }
+}
+
+void main() async {
+  String? lyrics = await GeniusService.fetchLyrics('Kaaris', 'Zoo');
+  print(lyrics);
 }
